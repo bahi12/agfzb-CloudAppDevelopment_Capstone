e2469a3f84ea0fad97bac527c87bda45bc8f375b
Hassan EL BAHI <me_80_me@hotmail.com>
2023-09-21 22:58:10 +0100

    Create models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
 # from .models import related models
+from .models import CarMake, CarModel
 
 
 # Register your models here.
 
 # CarModelInline class
-
+class CarModelInLine(admin.TabularInline):
+    model = CarModel
+    extra = 3
 # CarModelAdmin class
-
+class CarModelAdmin(admin.ModelAdmin):
+    fields = ("name", "make", "type", "year")
 # CarMakeAdmin class with CarModelInline
-
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInLine]
 # Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.timezone import now
-
+import datetime
+from django.urls import reverse
 
 # Create your models here.
 
@@ -11,6 +12,29 @@ from django.utils.timezone import now
 # - __str__ method to print a car make object
 
 
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=255)
+    description = models.TextField()
+
+    class Meta:
+        verbose_name =("Car Make")
+        verbose_name_plural =("Car Makes")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("CarMake_detail", kwargs={"pk": self.pk})
+
+
+    def get_absolute_url(self):
+        return reverse("CarMake_detail", kwargs={"pk": self.pk})
+
+class CarTypeField(models.CharField):
+    def get_choices(self):
+        car_makes = CarMake.objects.all()
+        choices = [(car_make.name, car_make.name) for car_make in car_makes]
+        return choices
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
 # - Name
@@ -19,6 +43,22 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    make = models.ForeignKey(CarMake , on_delete=models.CASCADE)
+    name = models.CharField(null=False, max_length=250)
+    dealer_id = models.IntegerField()
+    type = CarTypeField(max_length=255)
+    year = models.DateField(default=datetime.date.today().year)  
+      
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name =("Car Model")
+        verbose_name_plural =("Car Models")
+
+    def get_absolute_url(self):
+        return reverse("CarModel_detail", kwargs={"pk": self.pk})
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
